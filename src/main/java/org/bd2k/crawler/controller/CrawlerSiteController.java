2bcf5c9fc6b042712c9871a09b2479a3b75740f6
Allen G <alm.gong@gmail.com>
2016-06-23 16:43:19 -0700

    implement dropdown and radio buttons to navigate dashboard

--- src/main/java/org/bd2k/crawler/controller/CrawlerSiteController.java
+++ src/main/java/org/bd2k/crawler/controller/CrawlerSiteController.java
@@ -1,5 +1,7 @@
 package org.bd2k.crawler.controller;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.bd2k.crawler.model.User;
@@ -7,6 +9,7 @@ import org.bd2k.crawler.service.AuthService;
 import org.bd2k.crawler.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -41,13 +44,35 @@ public class CrawlerSiteController {
 	
 	/* dashboard */
 	@RequestMapping(value="/dashboard", method=RequestMethod.GET) 
-	public String getDashboardPage() {
+	public String getDashboardPage(Model model) {
+		//probably doesn't belong here, save for refactoring later
+//		HashMap<String, String> bd2kCenters = new HashMap<String, String>();
+//		bd2kCenters.put("BDDS", "https://bd2kccc.org/index.php/bdds");
+//		bd2kCenters.put("BDTG", "https://bd2kccc.org/index.php/bdtg");
+//		bd2kCenters.put("CCD", "https://bd2kccc.org/index.php/ccd");
+//		bd2kCenters.put("CEDAR", "https://bd2kccc.org/index.php/cedar");
+//		bd2kCenters.put("CPCP", "https://bd2kccc.org/index.php/cpcp");
+//		bd2kCenters.put("ENIGMA", "https://bd2kccc.org/index.php/enigma");
+//		bd2kCenters.put("HeartBD2K", "https://bd2kccc.org/index.php/heartbd2k");
+//		bd2kCenters.put("KnowEng", "https://bd2kccc.org/index.php/knoweng");
+//		bd2kCenters.put("LINCS-DCIC", "https://bd2kccc.org/index.php/lincs-dcic");
+//		bd2kCenters.put("LINCS-TG", "https://bd2kccc.org/index.php/lincs-tg");
+//		bd2kCenters.put("MD2K", "https://bd2kccc.org/index.php/md2k");
+//		bd2kCenters.put("Mobilize", "https://bd2kccc.org/index.php/mobilize");
+//		bd2kCenters.put("PIC-SURE", "https://bd2kccc.org/index.php/picsure");
+		
+		String[] bd2kCenters = {"BDDS", "BDTG", "CCD", "CEDAR", "CPCP", "ENIGMA",
+				"HeartBD2K", "KnowEng", "LINCS-DCIC", "LINCS-TG", "MD2K", "Mobilize",
+				"PIC-SURE"};
+		
+		model.addAttribute("bd2kCenters", bd2kCenters);
+		
 		return "dashboard";
 	}
 	
 	
 	// other
-	
+	//should be in its own controller
 	@RequestMapping(value="/login", method=RequestMethod.POST) 
 	public String loginUser(HttpServletRequest req) {
 		System.out.println("loginUser()");
--- src/main/webapp/WEB-INF/jsp/dashboard.jsp
+++ src/main/webapp/WEB-INF/jsp/dashboard.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -32,10 +35,32 @@
           <div class="container">
           <div class="row">
           	<div id="search-form" class="col-sm-12">
-          		<form action="" method="">
-          			<label for="search">Search by URL, or "all"</label>
-          			<input class="form-group" id="search" />
-          		</form>
+          		<div class="">
+          			<h4>Filter results below:</h4>
+          			<form action="" method="">
+          				<div class="form-group">
+          					<label for="center-select">Center:</label> 
+          					<select name="centerID" id="center-select">
+          						<option vale="all">All</option>
+          						<c:forEach items="${bd2kCenters}" var="center">
+          							<option value="${center}">${center}</option>
+          						</c:forEach>
+          					</select>
+          				</div>
+          				<div class="form-group">
+          					<label class="radio-inline">
+          						<input type="radio" name="type">Sites
+          					</label>
+          					<label class="radio-inline">
+          						<input type="radio" name="type">Publications
+          					</label>
+          					<label class="radio-inline">
+          						<input type="radio" name="type" checked="checked">All
+          					</label>
+          				</div>
+          			</form>
+          		</div>
+          		<hr />
           	</div>
 			</div>
 			<div class="row">	
