3935f2ed55e863a34fdccb005f729e5ef40982a0
Allen G <alm.gong@gmail.com>
2016-06-24 17:56:18 -0700

    update mongodb config file, and fix issue with properties resolution

--- src/main/java/org/bd2k/crawler/config/MongoConfig.java
+++ src/main/java/org/bd2k/crawler/config/MongoConfig.java
@@ -3,15 +3,19 @@ package org.bd2k.crawler.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
 @Configuration
+@PropertySource("classpath:app.properties")
 public class MongoConfig extends AbstractMongoConfiguration {
 	
-	@Value("${db.host}") String dbHost;
+	@Value("${db.host}")
+	String dbHost;
 	
 	@Override
 	public String getDatabaseName() {
@@ -22,5 +26,10 @@ public class MongoConfig extends AbstractMongoConfiguration {
 	@Bean
 	public Mongo mongo() throws Exception {
 		return new MongoClient(dbHost); 
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+	   return new PropertySourcesPlaceholderConfigurer();
 	}
 }
